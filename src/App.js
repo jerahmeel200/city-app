be63dd46c6a700b139f5e6c87db4ac4e26170ac9
jamico <jamicojerahmeel@gmailcom>
2023-06-24 20:57:26 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/header/Header";
 import "./App.css";
 import Hero from "./components/hero/Hero";
+import OurPartners from "./components/our-partners/OurPartners";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <OurPartners />
     </>
   );
 }
---
+++ src/components/our-partners/OurPartners.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "./OurPartners.css";
+
+function OurPartners() {
+  return (
+    <div
+      className="partContainer"
+      style={{ marginTop: "50px", height: "100%" }}
+    >
+      <h2
+        style={{ display: "flex", justifyContent: "center", fontWeight: 700 }}
+      >
+        Our Partners
+      </h2>
+      <div
+        style={{
+          //   background: "green",
+          width: "100%",
+          maxWidth: "900px",
+          display: "flex",
+          flexWrap: "wrap",
+          margin: "0 auto",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            // background: "red",
+            minWidth: "295px",
+            maxWidth: "295px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <img src="/images/afri.png" alt="" style={{ height: " 70px" }} />
+        </div>
+        <div
+          style={{
+            // background: "red",
+            minWidth: "295px",
+            maxWidth: "295px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <img src="/images/afri.png" alt="" style={{ height: " 70px" }} />
+        </div>
+        <div
+          style={{
+            // background: "red",
+            minWidth: "295px",
+            maxWidth: "295px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <img src="/images/afri.png" alt="" style={{ height: " 70px" }} />
+        </div>
+        <div
+          style={{
+            // background: "red",
+            minWidth: "295px",
+            maxWidth: "295px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <img src="/images/afri.png" alt="" style={{ height: " 70px" }} />
+        </div>
+        <div
+          style={{
+            // background: "red",
+            minWidth: "295px",
+            maxWidth: "295px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <img src="/images/afri.png" alt="" style={{ height: " 70px" }} />
+        </div>
+        <div
+          style={{
+            // background: "red",
+            minWidth: "295px",
+            maxWidth: "295px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <img src="/images/afri.png" alt="" style={{ height: " 70px" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OurPartners;
